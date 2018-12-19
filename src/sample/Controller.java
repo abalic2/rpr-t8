634c4d9fb3b51c7203396bbb52452d35116d8e17
abalic2 <abalic2@etf.unsa.ba>
2018-12-19 21:10:03 +0100

    nalazi datoteke

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,12 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 import java.io.File;
@@ -12,29 +15,30 @@ import java.io.IOException;
 public class Controller {
     public TextField tekst;
     public Button dugme;
+    public ListView lista;
+    private ObservableList<String> l;
+
+    public Controller() {
+        l = FXCollections.observableArrayList();
+    }
 
     public void initialize() {
+        lista.setItems(l);
     }
 
 
-    public static void displayDirectoryContents(File dir) {
-        try {
-            File[] files = dir.listFiles();
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    System.out.println("directory:" + file.getCanonicalPath());
-                    displayDirectoryContents(file);
-                } /*else {
-                        System.out.println("     file:" + file.getCanonicalPath());
-                    }*/
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void pretrazi(String put) {
+        File file = new File(put);
+        if (file.isDirectory()) {
+            if (file.listFiles() == null) return;
+            for (File f : file.listFiles()) pretrazi(f.getAbsolutePath());
+        } else if (file.isFile()) {
+            if (file.getName().contains(tekst.getText()))
+                l.add(file.getAbsolutePath());
         }
     }
 
     public void Trazi(ActionEvent actionEvent) {
-        File dat = new File(System.getProperty("user.home"));
-        displayDirectoryContents(dat);
+        pretrazi(System.getProperty("user.home"));
     }
 }
